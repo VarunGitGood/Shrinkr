96dd465978407a27d27568165e55df048023d871
Varun Singh <varun7singh10@gmail.com>
2023-08-09 02:55:58 +0530

    fixed completion

--- Makefile
+++ Makefile
@@ -1,8 +1,6 @@
 build:
 	@echo "Building shrinkr..."
 	@echo "Generating completion files..."
-	@go run main.go completion bash > comp.sh
 	@go build -o shrinkr
 	@sudo mv shrinkr /usr/local/bin/
-	@echo "Almost there..."
-	@echo "To enable autocomplete run: source comp.sh"
+	@echo "All done"
--- cmd/root.go
+++ cmd/root.go
@@ -12,12 +12,11 @@ import (
 
 // init a http client to make requests to the shrinkr server as it will be used in multiple commands
 
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "shrinkr",
 	Short: "Shinkr",
-	Long: `Shrinkr is a simple to use URL shortener with Google OAuth support and blazing fast performance.`,
+	Long:  `Shrinkr is a simple to use URL shortener with Google OAuth support and blazing fast performance.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -33,7 +32,6 @@ func Execute() {
 }
 
 func init() {
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
